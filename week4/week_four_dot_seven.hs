018f710abcff8cb86937d30d3a29001513b558e2
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-10-13 19:40:54 +0200

    wip parsec

---
+++ week4/week_four_dot_seven.hs
@@ -0,0 +1,30 @@
+Module main where
+
+data PersonRecord = MkPersonRecord {
+	name :: String,
+	address :: String,
+	id :: Integer,
+	labels :: Labels
+} deriving (Show)
+
+data Address = MkAddress {
+	line :: String,
+	number :: Integer,
+	street :: String,
+	town :: String,
+	postcode :: String
+} deriving(Show)
+
+data Label = Green | Red | Blue | Yello deriving(Show)
+
+rec1 = MkPersonRecord
+	"Gomez Guillaume"
+	MkAddress("University of compiegne", 8, "rue de la republique", "compiegne", "60200" )
+	0008090067
+	[Green, Red]
+
+rec1 = MkPersonRecord
+	"John Doe"
+	MkAddress("University of Joe", 8, "rue de l'inconnu", "Lost city", "00000" )
+	0008090067
+	[Green, Red, Yellow]
